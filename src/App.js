080e891972ff788e0f1e5b9c010533f4f459934e
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-07-13 13:17:33 -0700

    add component pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
+import {} from "module";
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Router>
           <Switch>
-            <Route exact path="/" component={home} />
-            <Route path="about" component={home} />
-            <Route path="contact" component={home} />
+            <Route exact path="/" component={Home} />
+            <Route path="about" component={About} />
+            <Route path="contact" component={Contact} />
             <Route component={NoMatch} />
           </Switch>
         </Router>
---
+++ src/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>This is mostly about us.</p>
+    <p>
+      Quia et quos animi.Eaque eius modi magnam possimus maxime sint.Perferendis
+      voluptas ducimus incidunt quam minus est quibusdam quibusdam.Magni dicta
+      aut velit mollitia nesciunt.Debitis ipsa aut harum velit possimus
+      deserunt.Ullam ipsum alias saepe.
+    </p>
+    <p>
+      Inventore quam vero voluptas.Accusantium distinctio quo qui nam nobis
+      error voluptatem.Unde et qui quisquam et aut.Qui sed iusto illo omnis
+      enim.Asperiores nesciunt enim impedit accusantium aut facere.
+    </p>
+    <p>
+      Id deserunt corrupti saepe ipsam porro ut culpa.Sit saepe amet dolorum
+      pariatur eum facilis adipisci reprehenderit.Corporis doloremque numquam
+      nihil sunt.Recusandae quam voluptates ut quia quod eum rerum modi.
+    </p>
+  </div>
+);
