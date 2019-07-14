d1a7a22e12d5f52a303394f19583add8b5f33263
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-07-13 18:54:48 -0700

    add bearsImage

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Jumbotron as Jumbo } from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+
+import styled from "styled-components";
+
+import bearsImage from "../assets/chicago_bears.jpg";
